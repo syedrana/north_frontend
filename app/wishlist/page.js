1c69484eb0cb53c02e162ef81ac619b3f3574ae7
Syed Rana <7syedrana@gmail.com>
2026-03-14 14:23:58 +0600

    modified wishlish page

--- app/wishlist/page.js
+++ app/wishlist/page.js
@@ -1,10 +1,24 @@
+import { cookies } from "next/headers";
 import { getWishlist } from "../../lib/wishlist";
 import WishlistPageClient from "./WishlistPageClient";
 
 export const dynamic = "force-dynamic";
 
 export default async function WishlistPage() {
-  const data = await getWishlist();
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
+
+  if (!token) {
+    return <WishlistPageClient initialItems={[]} initialTotalItems={0} />;
+  }
+
+  let data = { wishlist: [], totalItems: 0 };
+
+  try {
+    data = await getWishlist();
+  } catch (error) {
+    console.error("Failed to load wishlist:", error);
+  }
 
   return (
     <WishlistPageClient
